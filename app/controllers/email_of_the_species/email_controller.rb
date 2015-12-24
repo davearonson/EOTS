11432f26b28e2e62ac37d022fe4ad7c4fa434032
Dave Aronson <github.2.trex@codosaur.us>
2015-12-24 18:36:09 -0500

    refactor controller

--- app/controllers/email_of_the_species/email_controller.rb
+++ app/controllers/email_of_the_species/email_controller.rb
@@ -1,22 +1,46 @@
 class EmailOfTheSpecies::EmailController < ActionController::Base
 
-  def send
-    # TODO: FILTER WITH STRONG PARAMS!
-    klazz = params[:type].titleize.constantize
-    return unless klazz < EmailOfTheSpecies::Email
-    email = klazz.new(email_params)
-    # front-end requirements SHOULD make this redundant, BUT....
-    errs = email.errors
-    if errs.any?
-      flash.now[:alert] = errs.join("\n")
-      return false
-    end
-    Emailer.email(email).deliver
+  def send_email
+    Emailer.email(email).deliver if create_email && validate_email
     redirect_to root_path, notice: 'Your email has been sent'
   end
 
   def show
-    render(params[:type])
+    if create_email
+      render :show
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def create_email
+    # TODO: FILTER WITH STRONG PARAMS!
+    @type = params[:type]
+    errs = nil
+    msg = "Invalid email type #{@type}"
+    begin
+      klazz = "#{@type.camelize}Email".constantize
+    rescue
+      errs ||= [msg]
+    end
+    unless (klazz < EmailOfTheSpecies::Email)
+      errs ||= [msg]
+      @email = klazz.new(email_params)
+    end
+    flash_errors(errs)
+  end
+
+  def flash_errors(errs)
+    return true if errs.nil? || errs.none?
+    flash.now[:alert] = errs.join("\n")
+    false
+  end
+
+  def validate_email
+    errs = @email.errors
+    flash_errors(errs)
   end
 
 end
