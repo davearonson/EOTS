d1f13e347296886070fd7c2704b4a332821bc30c
Dave Aronson <github.2.trex@codosaur.us>
2015-12-24 18:35:42 -0500

    enginize

--- lib/email_of_the_species.rb
+++ lib/email_of_the_species.rb
@@ -1,17 +1,22 @@
+require "email_of_the_species/engine"
 require "email_of_the_species/version"
+
+# WTF, WE SHOULDN'T HAVE TO DO THIS!
+require_relative "../app/controllers/email_of_the_species/email_controller.rb"
 
 module EmailOfTheSpecies
 
-  # holds the field definitions of each class
-  FIELDS = {}
 
-  # to be used in routes.rb
-  def self.declare_type(type)
-    Rails.application.routes.draw do
-      # TODO: normalize exposed url
-      get "/contact/#{type}", :to => "email_of_the_species/email#show"
-      post "/contact/#{type}", :to => "email_of_the_species/email#send"
-    end
-  end
+  # holds the field definitions of each class
+  FIELDS = Hash.new { |h, k| h[k] = [] }
+
+# # to be used in routes.rb
+# def self.declare_type(type)
+#   Rails.application.routes.draw do
+#     # TODO: normalize exposed url
+#     get "/contact/#{type}", :to => "email_of_the_species/email#show"
+#     post "/contact/#{type}", :to => "email_of_the_species/email#send_email"
+#   end
+# end
 
 end
---
+++ lib/email_of_the_species/engine.rb
@@ -0,0 +1,5 @@
+module EmailOfTheSpecies
+  class Engine < Rails::Engine
+  end
+end
+
